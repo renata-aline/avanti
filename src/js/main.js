043aeae8f57b9e7a2ee9e6ec14e91da1efed21f3
renata <renatalinec@gmail.com>
2025-04-08 14:03:49 -0300

    adicionado seção de social media e accordion mobile

---
+++ src/js/main.js
@@ -0,0 +1,8 @@
+document
+  .querySelectorAll(".site-map__accordion_item-title")
+  .forEach((title) => {
+    title.addEventListener("click", () => {
+      const item = title.parentElement;
+      item.classList.toggle("active");
+    });
+  });
