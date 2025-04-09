80618f9fa63ac3601b337710222c5e1f13fac857
renata <renatalinec@gmail.com>
2025-04-09 16:19:27 -0300

    implementado lógica carrocel

--- src/js/main.js
+++ src/js/main.js
@@ -29,6 +29,21 @@ searchIcon.addEventListener("click", () => {
   }
 });
 
+// LOGIC FOR SLIDER
+
+const productWrapper = document.querySelector(".store__products");
+const btnLeft = document.getElementById("btnLeft");
+const btnRight = document.getElementById("btnRight");
+
+btnLeft.addEventListener("click", () => {
+  productWrapper.scrollLeft -= 200;
+  behavior: "smooth";
+});
+btnRight.addEventListener("click", () => {
+  productWrapper.scrollLeft += 200;
+  behavior: "smooth";
+});
+
 // LOGIC FOR FORM
 
 const form = document.getElementById("newsletterForm");
