e3a0de14990ed7dead1ea93a8ea6b214e2751846
renata <renatalinec@gmail.com>
2025-04-09 14:23:44 -0300

    adicionado lógica barra de busca

--- src/js/main.js
+++ src/js/main.js
@@ -9,6 +9,26 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 });
 
+// LOGIC FOR INPUTSEARCH
+
+const inputSearch = document.querySelector(".top-bar__search_input");
+const searchIcon = document.querySelector(".top-bar__search_icon");
+const searchMessage = document.getElementById("searchMessage");
+
+searchIcon.addEventListener("click", () => {
+  const produto = inputSearch.value.trim();
+  if (produto) {
+    searchMessage.textContent = `Você buscou por: "${produto}"`;
+    inputSearch.value = "";
+
+    setTimeout(() => {
+      searchMessage.textContent = "";
+    }, 3000);
+  } else {
+    searchMessage.textContent = "";
+  }
+});
+
 // LOGIC FOR FORM
 
 const form = document.getElementById("newsletterForm");
