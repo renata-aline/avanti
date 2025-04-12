c7be366647a349c34d1f6b26bc55c43be16de9ec
renata <renatalinec@gmail.com>
2025-04-11 23:31:27 -0300

    menu desktop

--- src/js/main.js
+++ src/js/main.js
@@ -9,7 +9,73 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 });
 
-// LOGIC FOR INPUTSEARCH
+// LOGIC FOR DESKTOP MENU
+let menus = [];
+fetch("/src/js/menu.json")
+  .then((response) => response.json())
+  .then((data) => {
+    menus = data.categories;
+  })
+  .catch((error) => {
+    console.error("Error fetching menu.json:", error);
+  });
+const menuDesktop = document.getElementById("menuDesktop");
+const menuList = document.getElementById("menulists");
+
+const menuCategoryTitle = document.getElementById("menuCategoryTitle");
+
+function classToggle(element, targetClass) {
+  element.classList.toggle(targetClass);
+}
+
+document.addEventListener("click", function (event) {
+  const isClickInside = menuDesktop.contains(event.target);
+  const isClickListNav = event.target.classList.contains(
+    "menu__nav_list-item-link"
+  );
+
+  if (
+    !isClickInside &&
+    !isClickListNav &&
+    menuDesktop.classList.contains("active")
+  ) {
+    menuDesktop.classList.remove("active");
+  }
+});
+
+function renderMenu(item) {
+  classToggle(menuDesktop, "active");
+  const listMenu = menus.filter((menu) => {
+    return menu.name === item;
+  });
+
+  let listHtml = "";
+  if (listMenu.length > 0) {
+    menuCategoryTitle.innerHTML = listMenu[0].category;
+    listMenu[0].links.forEach((menu) => {
+      const ul = document.createElement("ul");
+      ul.className = "menu-desktop__items_list";
+      let itens = "";
+
+      menu.links.forEach((link, index) => {
+        itens += `
+            <li class="menu-desktop__items_list-item"><a href="${
+              link.url
+            }" class="menu-desktop__items_list-item--link ${
+          index === 0 ? "menu-desktop__items_list-item--link--active" : ""
+        }">
+              ${link.name}
+            </a></li>
+          `;
+      });
+      ul.innerHTML = itens;
+      listHtml += ul.outerHTML;
+    });
+
+    menuList.innerHTML = listHtml;
+  }
+}
+// LOGIC FOR SEARCH
 
 const inputSearch = document.querySelector(".top-bar__search_input");
 const searchIcon = document.querySelector(".top-bar__search_icon");
@@ -36,11 +102,11 @@ const btnLeft = document.getElementById("btnLeft");
 const btnRight = document.getElementById("btnRight");
 
 btnLeft.addEventListener("click", () => {
-  productWrapper.scrollLeft -= 200;
+  productWrapper.scrollLeft -= 176;
   behavior: "smooth";
 });
 btnRight.addEventListener("click", () => {
-  productWrapper.scrollLeft += 200;
+  productWrapper.scrollLeft += 176;
   behavior: "smooth";
 });
 
